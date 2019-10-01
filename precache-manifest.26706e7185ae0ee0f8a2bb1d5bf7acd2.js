ee1d4715d1a8103284bb01ef5b319fb3c1fbaa42
Cloi <sircli@hotmail.com>
2019-09-30 23:31:15 -0300

    Updates

---
+++ precache-manifest.26706e7185ae0ee0f8a2bb1d5bf7acd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9c2966736e1ac900d4123087d597cc",
+    "url": "/uber-riders-community/index.html"
+  },
+  {
+    "revision": "b13d6b41046306087f1d",
+    "url": "/uber-riders-community/static/css/main.0af55fb7.chunk.css"
+  },
+  {
+    "revision": "bf37468dcb057f0fa8ca",
+    "url": "/uber-riders-community/static/js/2.f52e5e99.chunk.js"
+  },
+  {
+    "revision": "b13d6b41046306087f1d",
+    "url": "/uber-riders-community/static/js/main.7e81cafe.chunk.js"
+  },
+  {
+    "revision": "3d1573631f239c5f99a6",
+    "url": "/uber-riders-community/static/js/runtime-main.deb9892a.js"
+  }
+]);
