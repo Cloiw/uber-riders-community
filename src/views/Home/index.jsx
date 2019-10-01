6a3cb138227656b0be448c100d4d37264c9ee9ab
natiacostap <nati_aap@hotmail.com>
2019-10-01 16:46:38 -0300

    creamos Home con botones y mapa de la primera pantalla

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -6,11 +6,47 @@ import {
   Col,
   Button,
 } from 'react-bootstrap';
+import btnBusqueda from '../../img/btnBusqueda.png'
+import btnIniciar from '../../img/btnIniciar.png'
+import btnCondcutor from '../../img/btnConductor.png'
+import btnSeguridad from '../../img/btnSeguridad.png'
+import btnPanico from '../../img/btnPanico.png'
+import btnLocalizacion from '../../img/btnLocalizacion.png'
+
+
 
 const Home = () => (
-  <div>
-    <Maps />
-  </div>
+  <Container fluid>
+    <div class='mapaqlo'>
+      <Maps>
+
+      </Maps>
+    </div>
+    <Row>
+      <Col xs={6}>
+        <img src={btnBusqueda} />
+      </Col>
+      <Col xs={6}>
+        <img src={btnCondcutor} />
+      </Col>
+    </Row>
+
+    <Row md={12}>
+      <Col xs={4}>
+        <img src={btnSeguridad} />
+      </Col>
+      <Col xs={4}>
+        <img src={btnIniciar} />
+      </Col>
+      <Col xs={4}>
+        <img src={btnLocalizacion} />
+      </Col>
+    </Row>
+
+
+
+
+  </Container>
 )
 
 export default Home
