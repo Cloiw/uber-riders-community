9104b441fa1e26694c3b2dfec6cf2441f889da1c
natiacostap <nati_aap@hotmail.com>
2019-10-01 18:28:00 -0300

    pagina home con todos los botones

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -11,40 +11,48 @@ import btnIniciar from '../../img/btnIniciar.png'
 import btnCondcutor from '../../img/btnConductor.png'
 import btnSeguridad from '../../img/btnSeguridad.png'
 import btnPanico from '../../img/btnPanico.png'
-import btnLocalizacion from '../../img/btnLocalizacion.png'
-
-
+import btnLocalizacion from '../../img/btnLocalizacion.png';
+import btnEmergencia from '../../img/btnEmergencia.png';
+import CLP from '../../img/CLP.png'
+import './Home.css'
 
 const Home = () => (
   <Container fluid>
-    <div class='mapaqlo'>
-      <Maps>
-
-      </Maps>
-    </div>
     <Row>
-      <Col xs={6}>
-        <img src={btnBusqueda} />
-      </Col>
-      <Col xs={6}>
-        <img src={btnCondcutor} />
-      </Col>
+      <Maps/>
     </Row>
-
-    <Row md={12}>
-      <Col xs={4}>
-        <img src={btnSeguridad} />
-      </Col>
-      <Col xs={4}>
-        <img src={btnIniciar} />
-      </Col>
-      <Col xs={4}>
-        <img src={btnLocalizacion} />
-      </Col>
+    <div class='btns-top'> 
+      <Row xs={12}>
+        <Col xs={4}>
+          <img class='icons'src={btnBusqueda} />
+        </Col>
+        <Col xs={4}>
+          <img class='icons clp'src={CLP} />
+        </Col>
+        <Col xs={4}>
+          <img class='icons'src={btnCondcutor} />
+        </Col>
+      </Row>
+    </div>
+    <div class='btns-bottom'> 
+      <Row xs={12}>
+        <Col xs={4}>
+          <img id='security'class='icons'src={btnSeguridad} />
+          <img id='panic'class='icons'src={btnPanico} />
+        </Col>
+        <Col xs={4}>
+          <img class='icons'src={btnIniciar} />
+        </Col>
+        <Col xs={4}>
+          <img class='icons'src={btnLocalizacion} />
+        </Col>
+      </Row>
+    </div>
+    <Row xs={12}>
+      <Col> 
+        <a><img class='sos' src={btnEmergencia}></img> </a>
+      </Col> 
     </Row>
-
-
-
 
   </Container>
 )
