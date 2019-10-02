dea380cf8487d22cc6e27af51fe364c9278511d4
Stefany <efyguevara@gmail.com>
2019-10-01 21:56:19 -0300

    mostrando información del user y del reporte

--- src/views/Report/index.jsx
+++ src/views/Report/index.jsx
@@ -2,38 +2,83 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { db } from '../../data/firebase';
+import { Container, Row, Col } from 'react-bootstrap';
 
 
 class Report extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {}
-
-  }
+    }
 
   componentDidMount() {
-    const id = this.props.match.params
-   console.log(id.reportsId)
-   db.collection("pins").doc(id.reportsId).onSnapshot((querySnapshot)=>{
-     console.log(querySnapshot.data())
+    const id = this.props.match.params;
+
+    console.log(id);
+
+    db.collection("pins").doc(id.reportsId).onSnapshot((querySnapshot) => {
+      console.log(querySnapshot)
   
-    
-  })
-}
+      this.setState({
+        reportName: querySnapshot.data().title,
+        reportDate: querySnapshot.data().time,
+      })
 
-render() {
-  return (
-    <div className="report">
+    db.collection("users").doc(querySnapshot.data().author).onSnapshot((dataAccess) => {
+      console.log(dataAccess.data())
+      this.setState({
+        userId: dataAccess.data().id,
+        userCar: dataAccess.data().car_model,
+        userPlate: dataAccess.data().plate,
 
-      <p>ola</p>
-      <Link to="/">
-        <button>volver</button>
-      </Link>
+      })
+    })
+    })
 
-    </div>
-);
   }
-}
+
+  render() {
+ 
+    return (
+      <>       
+          <Row>
+            <Col xs={2} sm={2} md={2} xl={2}>
+              <Link to="/">
+                <button>Atrás</button>
+              </Link> 
+            </Col>
+
+            <Col xs={8} sm={8} md={8} xl={8}>
+              <p>{this.state.reportName}</p>
+            </Col>
+
+            <Col xs={2} sm={2} md={2} xl={2}>
+              <button>Menú</button>
+            </Col>  
+          </Row>
 
 
+          <Row>
+            <Col xs={6} md={6} xl={6}>
+              <p>Fecha:{this.state.reportDate}</p>
+            </Col>
+            <Col xs={6} md={6} xl={6}>
+              <p>Ubicacion:</p>
+            </Col>
+          </Row>
+
+
+          <Row>
+              <Col xs={4} md={4} xl={4}>
+                <p>Nombre de usuario:{this.state.userId}</p>
+                <p>Auto:{this.state.userCar}</p>
+                <p>Patente:{this.state.userPlate}</p>
+              </Col>
+          </Row>
+      </>
+        );
+      }
+    }
+    
+    
 export default Report;
