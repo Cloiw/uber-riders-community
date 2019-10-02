2e6413edbd562425556f56653facb8a23ca036b3
Stefany <efyguevara@gmail.com>
2019-10-02 01:09:58 -0300

    agregando archivo css  algunos e stilos para dferenciar las secciones de esta view

--- src/views/Report/index.jsx
+++ src/views/Report/index.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { db } from '../../data/firebase';
 import { Container, Row, Col } from 'react-bootstrap';
+import './report.css';
 
 
 class Report extends React.Component {
@@ -17,7 +18,7 @@ class Report extends React.Component {
     console.log(id);
 
     db.collection("pins").doc(id.reportsId).onSnapshot((querySnapshot) => {
-      console.log(querySnapshot)
+      console.log(querySnapshot);
   
       this.setState({
         reportName: querySnapshot.data().title,
@@ -41,24 +42,24 @@ class Report extends React.Component {
  
     return (
       <>       
-          <Row>
+          <Row className="bgColor">
             <Col xs={2} sm={2} md={2} xl={2}>
               <Link to="/">
-                <button>Atrás</button>
+                <button className="buttonBack"> Atrás</button>
               </Link> 
             </Col>
 
             <Col xs={8} sm={8} md={8} xl={8}>
-              <p>{this.state.reportName}</p>
+              <h3 className="fontWhite">{this.state.reportName}</h3>
             </Col>
 
             <Col xs={2} sm={2} md={2} xl={2}>
-              <button>Menú</button>
+              <button className="buttonBack" src="">Menú</button>
             </Col>  
           </Row>
 
 
-          <Row>
+          <Row className="bgColorWhite">
             <Col xs={6} md={6} xl={6}>
               <p>Fecha:{this.state.reportDate}</p>
             </Col>
@@ -68,12 +69,18 @@ class Report extends React.Component {
           </Row>
 
 
-          <Row>
+          <Row className="bgColorOpacity">
               <Col xs={4} md={4} xl={4}>
                 <p>Nombre de usuario:{this.state.userId}</p>
                 <p>Auto:{this.state.userCar}</p>
                 <p>Patente:{this.state.userPlate}</p>
               </Col>
+          </Row>
+          <Col xs={12} sm={12} md={12} xl={12}>
+            <h4>Comentarios</h4>
+          </Col>
+          <Row>
+
           </Row>
       </>
         );
