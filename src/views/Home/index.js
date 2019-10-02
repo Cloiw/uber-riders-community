6d7cb6bbfaee7ba013b205fa317b76f61ca98f3a
natiacostap <nati_aap@hotmail.com>
2019-10-02 08:55:39 -0300

    cambios en Home con Css'

---
+++ src/views/Home/index.js
@@ -0,0 +1,60 @@
+// import React from 'react';
+// import Maps from '../../components/Maps';
+// import {
+//   Container,
+//   Row,
+//   Col,
+//   Button,
+// } from 'react-bootstrap';
+// import btnBusqueda from '../../img/btnBusqueda.png'
+// import btnIniciar from '../../img/btnIniciar.png'
+// import btnCondcutor from '../../img/btnConductor.png'
+// import btnSeguridad from '../../img/btnSeguridad.png'
+// import btnPanico from '../../img/btnPanico.png'
+// import btnLocalizacion from '../../img/btnLocalizacion.png';
+// import btnEmergencia from '../../img/btnEmergencia.png';
+// import CLP from '../../img/CLP.png'
+// import './Home.css'
+
+// const Home = () => (
+//   <Container fluid>
+//     <Row>
+//       <Maps/>
+//     </Row>
+//     <div class='btns-top'> 
+//       <Row xs={12}>
+//         <Col xs={4}>
+//           <img class='icons'src={btnBusqueda} />
+//         </Col>
+//         <Col xs={4}>
+//           <img class='icons clp'src={CLP} />
+//         </Col>
+//         <Col xs={4}>
+//           <img class='icons'src={btnCondcutor} />
+//         </Col>
+//       </Row>
+//     </div>
+//     <div class='btns-bottom'> 
+//       <Row xs={12}>
+//         <Col xs={4}>
+//           <img id='security'class='icons'src={btnSeguridad} />
+//           <img id='panic'class='icons'src={btnPanico} />
+//         </Col>
+//         <Col xs={4}>
+//           <img class='icons'src={btnIniciar} />
+//         </Col>
+//         <Col xs={4}>
+//           <img class='icons'src={btnLocalizacion} />
+//         </Col>
+//       </Row>
+//     </div>
+//     <Row xs={12}>
+//       <Col> 
+//         <a><img class='sos' src={btnEmergencia}></img> </a>
+//       </Col> 
+//     </Row>
+
+//   </Container>
+// )
+
+// export default Home
